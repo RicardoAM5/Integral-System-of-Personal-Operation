20b1945aa7e65477d0ff80dd0d3743e83b859482
Ricardo Alonso Medina <ricardoalonsomedina99@gmail.com>
2024-02-25 16:08:01 -0600

    Init

---
+++ ispo/src/main/java/com/idgs/ispo/IspoApplication.java
@@ -0,0 +1,13 @@
+package com.idgs.ispo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IspoApplication.class, args);
+	}
+
+}
---
+++ ispo/src/test/java/com/idgs/ispo/IspoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idgs.ispo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IspoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
